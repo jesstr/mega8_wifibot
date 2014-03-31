32133b9bfad0134a7f42a8029764ff8a8344df4d
openwrt <jesstr@mail.ru>
2014-03-31 17:58:24 +0400

    turret dc control workflow..

--- turret.c
+++ turret.c
@@ -13,20 +13,51 @@
 /* General turret initialization */
 void Turret_Init(void)
 {
-	GUN_INIT;
+	//GUN_INIT;
 	LASER_INIT;
 }
 
 /* Start fire according to passed duration */
 void Turret_Fire(unsigned char duration)
 {
+/* TODO Change realization without using l293 chip */
+#if 0
 	TURRET_GUN_FIRE;
 	/* TODO Non-blocking delay */
 	wait_ms(duration);
 	TURRET_GUN_STOPFIRE
+#endif
 }
 
 /* Turret moving according to passed horizontal and vertical pulse widths */
+/* If DC motor is used, it should be controlled like servo, *
+ * passing pulse width. That's for compatibility. */
+void Turret_Move(unsigned short hor_pos, unsigned short vert_pos)
+{
+	if ((SERVO_MAX_PULSE_WIDTH + 1 > hor_pos) &&
+			( hor_pos > SERVO_MIN_PULSE_WIDTH - 1)) {
+
+#ifdef _HOR_DC_
+	/* TODO Horizontal DC run  */
+
+#else
+#ifdef _HOR_SERVO_
+		servo_pulse_width[TURR_HOR_SERVO] = hor_pos;
+#endif
+#endif
+
+	}
+	if ((SERVO_MAX_PULSE_WIDTH + 1 > vert_pos) && ( vert_pos > SERVO_MIN_PULSE_WIDTH - 1)) {
+		servo_pulse_width[TURR_VERT_SERVO] = vert_pos;
+	}
+	Servo_UpdateArrays();
+}
+
+/* TODO remove */
+#if 0
+/* Turret moving according to passed horizontal and vertical pulse widths */
+/* Horizontal move: servo
+ * Vertical move: servo */
 void Turret_Move(unsigned short hor_pos, unsigned short vert_pos)
 {
 	if ((SERVO_MAX_PULSE_WIDTH * 2 + 1 > hor_pos + vert_pos) && (hor_pos + vert_pos > SERVO_MIN_PULSE_WIDTH * 2 - 1)) {
@@ -35,3 +66,4 @@ void Turret_Move(unsigned short hor_pos, unsigned short vert_pos)
 		Servo_UpdateArrays();
 	}
 }
+#endif
--- turret.h
+++ turret.h
@@ -11,6 +11,12 @@
 #include <avr/io.h>
 #include "l293.h"
 
+/* Definition of turret mechanics. Turret mechanics scheme which is actually used should be uncommented. */
+#define _HOR_DC_ 			/* DC motor for horizontal moving, servo for vertical moving */
+/* #define _HOR_SERVO_ */	/* Servo for horizontal moving, servo for vertical moving */
+
+/* TODO Change realization without using l293 chip */
+#if 0
 #define GUN_DISABLE				CHIP2_DISABLE
 #define GUN_ENABLE				CHIP2_ENABLE
 
@@ -26,6 +32,41 @@
 								GUN_DISABLE;					\
 								CE2_DDR |= (1<<CE2_PIN);		\
 								} while(0)
+#endif
+
+#ifdef _HOR_DC_
+#define MOTOR_HOR_DISABLE		CHIP2_DISABLE
+#define MOTOR_HOR_ENABLE		CHIP2_ENABLE
+
+#define MOTOR_HOR_LEFT			do { 							\
+								INPUT3_PORT |= (1<<INPUT3_PIN);	\
+								INPUT4_PORT &= ~(1<<INPUT4_PIN);\
+								} while(0)
+
+#define MOTOR_HOR_RIGH			do { 							\
+								INPUT4_PORT |= (1<<INPUT4_PIN);	\
+								INPUT3_PORT &= ~(1<<INPUT3_PIN);\
+								} while(0)
+
+#define TURRET_HOR_LEFT_RUN		do { 				\
+								MOTOR_HOR_LEFT		\
+								MOTOR_HOR_ENABLE	\
+								} while(0)
+
+#define TURRET_HOR_RIGHT_RUN	do { 				\
+								MOTOR_HOR_RIGHT		\
+								MOTOR_HOR_ENABLE	\
+								} while(0)
+
+#define TURRET_HOR_STOP			MOTOR_HOR_DISABLE
+
+#define TURRET_MOTOR_HOR_INIT	do {							\
+								MOTOR_HOR_DISABLE;				\
+								CE2_DDR |= (1<<CE2_PIN);		\
+								INPUT3_DDR |= (1<<INPUT3_PIN);	\
+								INPUT4_DDR |= (1<<INPUT4_PIN);	\
+								} while(0)
+#endif
 
 #define LASER_PIN				PB0
 #define LASER_PORT 				PORTB
@@ -38,7 +79,6 @@
 								TURRET_LASER_OFF;				\
 								LASER_DDR |= (1<<LASER_PIN);	\
 								} while(0)
-
 
 /* General turret initialization */
 void Turret_Init(void);
